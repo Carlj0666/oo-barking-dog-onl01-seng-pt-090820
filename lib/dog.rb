7509dd7f773525988e502c9ed54919b88f11f6e9
Carl J <carlj1881@yahoo.com>
2020-09-21 20:23:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,15 @@
 class Dog.new
   def name=(dog_name)
-    
+    @this_dogs_name = dogs_name
+    @dog_bark
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
 end
 
-def name
-  this_dogs_name
-end
-end
